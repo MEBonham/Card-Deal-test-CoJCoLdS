34ff5ba8fd498985a38b748aadb8563addc636cb
MEBonham <programmer@mckaybonham.com>
2022-05-11 12:33:01 -0600

    Project complete (I think)

--- README.md
+++ README.md
@@ -6,4 +6,7 @@
 4. Set up variables, layout in JSX
 5. CSS (display: flex; line, widths); don't forget to `import` css file!
 6. new Terminal: `cd carddeal` `yarn` `yarn dev`
-7. 
+7. new Terminal: `cd carddeal` `yarn add mersenne-twister`
+8. `yarn add lodash` (optional, didn't end up using)
+9. `shuffle()` function following idea at https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+10. useEffect hook to handle asynchronicity of shuffle setting; deal cards to players.
--- carddeal/src/App.jsx
+++ carddeal/src/App.jsx
@@ -1,8 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import MersenneTwister from 'mersenne-twister';
 
 import './App.css'
 
 const App = () => {
+    const CARDS_TO_DEAL = 5;
+    const gen = new MersenneTwister();
 
     const suits = ["Clubs", "Diamonds", "Hearts", "Spades"];
     const values = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
@@ -35,23 +38,47 @@ const App = () => {
             hand: []
         }
     ]);
+    // console.log(players);
 
     const shuffle = () => {
+        // Give each card in the deck a random value in the interval [0,1).
+        // Sort the array of cards according to those random values.
+        // Then drop the random values, retaining only the cards' original data.
+        // See https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array for idea origin and alternative algorithms.
+        setDeckShuffled(deckInOrder
+            .map((cardObj) => ({ cardObj, sortIdx: gen.random() }))
+            .sort((a, b) => a.sortIdx - b.sortIdx)
+            .map(({ cardObj }) => cardObj)
+        );
+    }
+    // Anytime the deck gets Shuffled, re-deal to players (handling asynchronicity with useEffect dependency array)
+    useEffect(() => {
+        const numPlayers = players.length;
 
-    }
-    const deal = () => {
-        
-    }
-    const shuffleAndDeal = () => {
-        shuffle();
-        deal();
-    }
+        if (deckShuffled.length > 0) {
+            setPlayers(players
+                .map((playerObj, playerIdx) => {
+                    const tempHand = [];
+                    // Deal cards from the shuffled deck in the order they would be given out in physically dealing the cards, just for realism.
+                    // For example, with four players, the first player dealt to will get cards number 1, 5, 9, 13, and 17 from the deck.
+                    for (let i = playerIdx; i < CARDS_TO_DEAL * numPlayers; i += numPlayers) {
+                        tempHand.push(deckShuffled[i]);
+                    }
+    
+                    return({
+                        ...playerObj,
+                        hand: tempHand
+                    });
+                })
+            );
+        }
+    }, [ deckShuffled ]);
 
     return(
         <div className="App">
             <div className="display">
                 {players.map((playerObj) => (
-                    <section>
+                    <section key={playerObj.name}>
                         <h1>{playerObj.name}</h1>
                         <ul>
                             {playerObj.hand.map((cardObj) => (
@@ -61,7 +88,7 @@ const App = () => {
                     </section>
                 ))}
             </div>
-            <button onClick={shuffleAndDeal}>Shuffle &amp; Deal</button>
+            <button onClick={shuffle}>Shuffle &amp; Deal</button>
         </div>
     );
 }
